bd0194b7850d902a7956519000ef1cccdb733f2a
reflutter <cpollet@users.noreply.github.com>
2024-02-15 20:45:03 +0100

    wip

---
+++ test.m
@@ -0,0 +1,24 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA, see http://wlplugin.halirutan.de/ *)
+
+(* :Title: test *)
+(* :Context: test` *)
+(* :Author: cpollet *)
+(* :Date: 2020-05-02 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: 11.3 *)
+(* :Copyright: (c) 2020 cpollet *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["test`"];
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"];
+
+End[]; (* `Private` *)
+
+
+EndPackage[]
+
